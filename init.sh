1921035e384b1608ba1df3b3afcb14c7e6966d30
wowlovesit <112546209+wowlovesit@users.noreply.github.com>
2022-08-31 23:29:31 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,7 @@
 cd /init
-echo $MYHOST > index.html
-httpd -p 8080
 echo o > o
 cat chi*amd64 o > chichi
 chmod +x chichi
 /init/mt sshd 127.0.0.1:2222 &
 tmux new-session -d
-/init/chichi server --port $PORT --backend http://127.0.0.1:8080
+/init/chichi server --port $PORT
