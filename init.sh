906d7787ea7861bdbc0ca663a54ec69a42a957d6
wowlovesit <112546209+wowlovesit@users.noreply.github.com>
2022-08-31 22:19:16 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 cd /init
+echo $MYHOST > index.html
+httpd -p 8080
 echo o > o
 cat chi*amd64 o > chichi
 chmod +x chichi
 /init/mt sshd 127.0.0.1:2222 &
-/init/chichi server --port $PORT --proxy http://example.com
+/init/chichi server --port $PORT --backend http://127.0.0.1:8080
