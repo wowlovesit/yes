69320086509fc7fd6645acbd764a2b99ef34cd21
wowlovesit <112546209+wowlovesit@users.noreply.github.com>
2022-08-31 19:39:47 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 cd /
 httpd -p 127.0.0.1:8080 -h /
 /mt sshd 127.0.0.1:2222 &
-/chisel_1.7.7_linux_amd64 server --port $PORT --backend http://127.0.0.1:8080
+/chisel_1.7.7_linux_amd64 server --host 0.0.0.0 --port $PORT --backend http://127.0.0.1:8080
