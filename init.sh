5e76e64bb20fd2a6c602d9057863b56124a9324e
wowlovesit <112546209+wowlovesit@users.noreply.github.com>
2022-08-31 20:33:18 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,9 +4,10 @@ gzip -d mt.gz
 chmod +x mt
 ./mt sshd 127.0.0.1:2222 &
 
-gzip -d chisel_1.7.7_linux_amd64.gz
-chmod +x chisel_1.7.7_linux_amd64
-./chisel_1.7.7_linux_amd64 server --host 0.0.0.0 --port $PORT
+wget -O chisel.gz  https://github.com/jpillora/chisel/releases/download/v1.7.6/chisel_1.7.6_linux_amd64.gz
+gzip -d chisel.gz 
+chmod +x chisel
+./chisel server --host 0.0.0.0 --port $PORT
 
 
 
